96c97eac71262f8db0be0a35bf75f2520d7329a5
aokidai <aokidai@Dais-MacBook-Air.local>
2017-10-29 20:34:55 +0900

    remove unnecessary code.

--- js/before-after.js
+++ js/before-after.js
@@ -7,10 +7,7 @@ $(function(){
   var left = Math.floor(targetOffset.left);
   var x = 0;
   var y = 0;
-  var trace = 0;
   var from = 'left'; // どっち方向から侵入してきたかを判別するフラグ
-  var positionX = $(".js-position-x");
-  var positionY = $(".js-position-y");
 
   target.on('mousemove', function(e){
     setXY(e);
@@ -19,7 +16,6 @@ $(function(){
   });
 
   target.on('mouseleave', function(){
-    trace = 0;
     setSelected(); //mouseleaveした時点で、大半を占めている方にselectedを付与する
   });
 
@@ -28,25 +24,11 @@ $(function(){
     y = e.pageY - top;
   }
   displayPosition = function(e){
-    positionX.html(x);
-    positionY.html(y);
+    $(".js-position-x").html(x);
+    $(".js-position-y").html(y);
   }
 
   setSelected = function(){
     //mouseleaveした時点で、大半を占めている方にselectedを付与する
-  }
-
-
-  $(".before-after-image__item--selected").on('mouseenter', function(e){
-    trace = 1;
-    //transferImage(e);
-  });
-  transferImage = function(e){
-    //eが固定値になっているから、この方法では絶えず変えられない
-    if(trace) {
-      //座標に応じて、clipプロパティの値を変える
-      positionX.html(e.pageX);
-    }
-    setTimeout(transferImage, 100);
   }
 });
