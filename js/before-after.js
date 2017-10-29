bca900cb358e6db46847a7e2efc19ee8c26eea12
aokidai <aokidai@Dais-MacBook-Air.local>
2017-10-29 22:46:19 +0900

    set z-index. refactor.

--- js/before-after.js
+++ js/before-after.js
@@ -1,34 +1,61 @@
+/*
+ * 現状、X方向のスライドにしか対応していないが、
+ * 侵入方向の判定を追加することで、Y方向のスライドにも対応可能
+ */
+function setZindex() {
+  count = $(".before-after-image__item").length;
+  $(".before-after-image__item").each(function(i) {
+    $(this).css("z-index", count-i);
+  });
+}
+
 $(function(){
   var target = $(".before-after-image");
   var targetOffset = target.offset();
-  var top = Math.floor(targetOffset.top);
-  var right = Math.floor(target.width());
-  var bottom = Math.floor(target.height());
-  var left = Math.floor(targetOffset.left);
   var x = 0;
   var y = 0;
-  var from = 'left'; // どっち方向から侵入してきたかを判別するフラグ
+
+  var IMAGEDIR = {
+    TOP:    Math.floor(targetOffset.top),
+    RIGHT:  Math.floor(target.width()),
+    BOTTOM: Math.floor(target.height()),
+    LEFT:   Math.floor(targetOffset.left)
+  };
+  var FROMDIR = {
+    UP:    0,
+    DOWN:  1,
+    RIGHT: 2,
+    LEFT:  3
+  };
+
+  setZindex();
 
   target.on('mousemove', function(e){
     setXY(e);
     displayPosition(e);
-    $(".before-after-image__item--selected").css("clip", "rect(0px," + right + "px," + bottom + "px," + x + "px)");
+    $(".before-after-image__item--selected").css("clip", "rect(0px," + IMAGEDIR.RIGHT + "px," + IMAGEDIR.BOTTOM + "px," + x + "px)");
   });
 
   target.on('mouseleave', function(){
-    setSelected(); //mouseleaveした時点で、大半を占めている方にselectedを付与する
+    //clipを段階的にすることで、ぬるっとできる
+    if(x > IMAGEDIR.RIGHT/2) {
+      var selected = $(".before-after-image__item--selected");
+      selected.css("display", "none");
+      selected.removeClass("before-after-image__item--selected");
+      var next = selected.next(".before-after-image__item");
+      next.addClass("before-after-image__item--selected");
+    }
+    else {
+      // 左側の場合、現在画像でstay
+    }
   });
 
   setXY = function(e){
-    x = e.pageX - left;
-    y = e.pageY - top;
+    x = e.pageX - IMAGEDIR.LEFT;
+    y = e.pageY - IMAGEDIR.TOP;
   }
   displayPosition = function(e){
     $(".js-position-x").html(x);
     $(".js-position-y").html(y);
-  }
-
-  setSelected = function(){
-    //mouseleaveした時点で、大半を占めている方にselectedを付与する
   }
 });
